8d5b832c71f25043ac73222be32d9041f604a30c
Nguyễn Quốc Đạt <datfeed.2706@gmail.com>
2025-09-27 21:43:18 +0700

    fix helpers

--- helpers.php
+++ helpers.php
@@ -9,3 +9,18 @@ function dd($data) {
     echo '</pre>';
     die();
 }
+
+function isJson($string) {
+    json_decode($string);
+    return (json_last_error() === JSON_ERROR_NONE);
+}
+
+function slugify($text) {
+    $text = iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $text);
+    $text = strtolower($text);
+
+    $text = preg_replace('/[^a-z0-9]+/i', '-', $text);
+    $text = trim($text, '-');
+
+    return $text;
+}
